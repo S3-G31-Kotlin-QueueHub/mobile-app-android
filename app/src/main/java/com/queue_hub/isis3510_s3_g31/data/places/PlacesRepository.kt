c3b41f25ba01787088bd4c99a2d363c009ff01e6
Pacolas <160373515+Pacolas@users.noreply.github.com>
2024-10-23 00:39:46 -0500

    Actualizacion

--- app/src/main/java/com/queue_hub/isis3510_s3_g31/data/places/PlacesRepository.kt
+++ app/src/main/java/com/queue_hub/isis3510_s3_g31/data/places/PlacesRepository.kt
@@ -11,17 +11,17 @@ class PlacesRepository (
     private  val api: PlacesApi,
     private var place: Places = Places(
         id = "1",
-        idFranquicia = "Franquicia_123",
-        nombre = "Café del Sol",
-        direccion = "Calle 123, Bogotá",
-        telefono = "555-1234",
+        idFranquicia = "Unknown",
+        nombre = "No name",
+        direccion = "No address",
+        telefono = "000-000-000",
         latitud = 4.60971,
         longitud = -74.08175,
         urlImg = "https://24ai.tech/es/wp-content/uploads/sites/5/2023/10/01_product_1_sdelat-kvadratnym-3-1.jpg",
-        averageWaitingTime = 15,
-        averageWaitingTimeLastHour = 10,
-        averageScoreReview = 4.5f,
-        bestAverageFrame = "12:00-14:00"
+        averageWaitingTime = 0,
+        averageWaitingTimeLastHour = 0,
+        averageScoreReview = 0f,
+        bestAverageFrame = "empty"
     )
 ){
 
