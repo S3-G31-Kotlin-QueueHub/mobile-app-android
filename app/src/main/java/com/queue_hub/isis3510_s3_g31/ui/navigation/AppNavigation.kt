6d46c8ba5d67e6c14dd5629f99e7563a2c66855d
samueljimenezdev <sajimenezr1@gmail.com>
2024-11-14 17:21:25 -0500

    final localization without display and calculate distance

--- app/src/main/java/com/queue_hub/isis3510_s3_g31/ui/navigation/AppNavigation.kt
+++ app/src/main/java/com/queue_hub/isis3510_s3_g31/ui/navigation/AppNavigation.kt
@@ -1,6 +1,6 @@
 package com.queue_hub.isis3510_s3_g31.ui.navigation
 
-import com.queue_hub.isis3510_s3_g31.ui.screens.login.LoginViewModel
+import android.content.Context
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
@@ -9,6 +9,7 @@ import androidx.navigation.compose.rememberNavController
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.queue_hub.isis3510_s3_g31.MainScreen
+import com.queue_hub.isis3510_s3_g31.MainViewModel
 import com.queue_hub.isis3510_s3_g31.data.places.PlacesRepository
 import com.queue_hub.isis3510_s3_g31.data.queues.QueuesRepository
 import com.queue_hub.isis3510_s3_g31.data.turns.TurnsRepository
@@ -19,6 +20,7 @@ import com.queue_hub.isis3510_s3_g31.ui.screens.detail.DetailViewModel
 import com.queue_hub.isis3510_s3_g31.ui.screens.home.HomeScreen
 import com.queue_hub.isis3510_s3_g31.ui.screens.home.HomeViewModel
 import com.queue_hub.isis3510_s3_g31.ui.screens.login.LoginScreen
+import com.queue_hub.isis3510_s3_g31.ui.screens.login.LoginViewModel
 import com.queue_hub.isis3510_s3_g31.ui.screens.profile.ProfileScreen
 import com.queue_hub.isis3510_s3_g31.ui.screens.profile.ProfileViewModel
 import com.queue_hub.isis3510_s3_g31.ui.screens.recommended.RecommendedScreen
@@ -27,6 +29,7 @@ import com.queue_hub.isis3510_s3_g31.ui.screens.signup.SignUpScreen
 import com.queue_hub.isis3510_s3_g31.ui.screens.signup.SignUpViewModel
 import com.queue_hub.isis3510_s3_g31.ui.screens.wait.WaitScreen
 import com.queue_hub.isis3510_s3_g31.ui.screens.wait.WaitViewModel
+import com.queue_hub.isis3510_s3_g31.utils.location_services.LocationProvider
 
 
 @Composable
@@ -38,8 +41,11 @@ fun AppNavigation(
     userPreferencesRepository: UserPreferencesRepository,
     turnsRepository: TurnsRepository,
     startDestination: Any,
-    queuesRepository: QueuesRepository
+    queuesRepository: QueuesRepository,
+    context : Context,
+    mainViewModel: MainViewModel
 ) {
+
     val navController = rememberNavController()
 
     NavHost(
@@ -50,10 +56,10 @@ fun AppNavigation(
            LoginScreen(viewModel = LoginViewModel(usersRepository = userRepository, userPreferencesRepository = userPreferencesRepository) , navController = navController, auth = auth )
         }
         composable<Main> {
-            MainScreen(navController = navController, placesRepository = placesRepository, userPreferencesRepository = userPreferencesRepository, turnsRepository = turnsRepository, queuesRepository = queuesRepository )
+            MainScreen(navController = navController, placesRepository = placesRepository, userPreferencesRepository = userPreferencesRepository, turnsRepository = turnsRepository, queuesRepository = queuesRepository, context = context, locationProvider = LocationProvider(context), mainViewModel = mainViewModel )
         }
         composable<Home> {
-            HomeScreen(navController = navController, modifier = Modifier, homeViewModel = HomeViewModel( placesRepository, userPreferencesRepository ), placesRepository = placesRepository)
+            HomeScreen(navController = navController, modifier = Modifier, homeViewModel = HomeViewModel( placesRepository, userPreferencesRepository ), placesRepository = placesRepository, location = null)
         }
         composable<Recommended>{
             RecommendedScreen(navController = navController, recommendedViewModel = RecommendedViewModel( placesRepository ), repository = placesRepository)
