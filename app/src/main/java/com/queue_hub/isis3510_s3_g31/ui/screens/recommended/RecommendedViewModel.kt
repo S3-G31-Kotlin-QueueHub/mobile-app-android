6856ef0191ad33700d970c49740457ba31cc645b
samueljimenezdev <sajimenezr1@gmail.com>
2024-11-16 11:28:08 -0500

    feature/facade-data-layer-pattern

--- app/src/main/java/com/queue_hub/isis3510_s3_g31/ui/screens/recommended/RecommendedViewModel.kt
+++ app/src/main/java/com/queue_hub/isis3510_s3_g31/ui/screens/recommended/RecommendedViewModel.kt
@@ -1,31 +1,45 @@
 package com.queue_hub.isis3510_s3_g31.ui.screens.recommended
 
+import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.queue_hub.isis3510_s3_g31.data.DataLayerFacade
 import com.queue_hub.isis3510_s3_g31.data.places.PlacesRepository
+import com.queue_hub.isis3510_s3_g31.data.places.model.Place
+import com.queue_hub.isis3510_s3_g31.ui.screens.home.HomeViewState
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 
-class RecommendedViewModel ( private val placesRepository: PlacesRepository): ViewModel() {
+class RecommendedViewModel ( private val dataLayerFacade: DataLayerFacade): ViewModel() {
+
 
     var state by mutableStateOf(RecommendedViewState())
         private set
 
     init {
-        viewModelScope.launch {
-            delay(100)
+        getAllPlaces()
+    }
+
+    private fun getAllPlaces(){
+
+        viewModelScope.launch (Dispatchers.IO){
+            val allPlaces : List<Place> = dataLayerFacade.getAllPlaces()
+            allPlaces.forEach {
+                Log.d("DatosPlaces", "Place vm: $it")
+            }
             state = state.copy(
-                places = placesRepository.getRecommendedPlaces(),
+                places = allPlaces,
                 isLoading = false
             )
         }
     }
-
-
-
 
 }
 
